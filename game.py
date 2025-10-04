0e90ae96f14b788aa0c970843882354b9674fa2d
eetu <eetu.vaananen@metropolia.fi>
2025-10-04 17:57:05 +0300

    commit

--- game.py
+++ game.py
@@ -1,5 +1,12 @@
 
+#fuktio, jolla näkee oman rahatilanteensa:
+
+#kiitorata osto funktio:
+
+#terminaali osto funktio:
+
 #kiitorata päivitys funktio:
 
 #terminaali päivitys fuktio:
 
+#funktio, joka aloittaa seuraaan vuoron:
