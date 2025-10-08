86a9c10e6ff734b88bc367ccdabe3cfc81fef65c
ptpar <pinja.t.parviainen@gmail.com>
2025-10-08 16:38:42 +0300

    palautettu alkuperäiseksi

--- functions/buy_runway.py
+++ functions/buy_runway.py
@@ -6,31 +6,29 @@ from functions.get_airport_id import get_airport_id
 def buy_runway(connect, player, icao):
     balance = give_bank_balance(connect, player)
     airport_type = get_airport_type(connect, icao)
-
     if airport_type == "small_airport":
         runway_id = 0
+        runway_cost = 3_000_000
+        if balance < runway_cost:
+            print("You don't have enough money!")
+            return
     elif airport_type == "medium_airport":
         runway_id = 1
+        runway_cost = 6_000_000
+        if balance < runway_cost:
+            print("You don't have enough money!")
+            return
     else:
         runway_id = 2
-
-    cursor = connect.cursor(dictionary=True)
-    cursor.execute("select length, cost, construction_time, operating_cost, yield from runway_types where id=%s",
-                   (runway_id,))
-    runway = cursor.fetchone()  # kiitoradan tiedot sanakirjana, sarakkeiden nimet ovat sanakirjan avaimia
-
-    if balance < runway["cost"]:
-        print("You don't have enough money!")
-        return
-
+        runway_cost = 9_000_000
+        if balance < runway_cost:
+            print("You don't have enough money!")
+            return
     airport_id = get_airport_id(connect, player, icao)
     cursor = connect.cursor()
     cursor.execute("""
     insert into player_runway (player_airports_id, runway_types_id) values (%s, %s)
                    """, (airport_id, runway_id))
-    # Päivitetään player_airports.yield
-    sum_yield = runway["yield"] - runway["operating_cost"]
-    cursor.execute("UPDATE player_airports SET yield = yield + %s WHERE id = %s", (sum_yield, airport_id))
-    cursor.execute("update game set money = money - %s where name = %s", (runway["cost"], player,))
+    cursor.execute("update game set money = money - %s where name = %s", (runway_cost, player,))
     print("Runway purchase successful!")
     cursor.close()
