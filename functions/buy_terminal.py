e7ea49df613f44287e8e60f2094aa6aec0ff1049
ptpar <pinja.t.parviainen@gmail.com>
2025-10-08 16:01:55 +0300

    player_airports.yield päivitys
    Ostetun terminaalin operating_cost ja yield lisätty player_airports.yield:iin

--- functions/buy_terminal.py
+++ functions/buy_terminal.py
@@ -5,30 +5,30 @@ from functions.get_airport_id import get_airport_id
 def buy_terminal(connect, player, icao):
     balance = give_bank_balance(connect, player)
     airport_type = get_airport_type(connect, icao)
+
     if airport_type == "small_airport":
         terminal_id = 0
-        terminal_cost = 3_000_000
-        if balance < terminal_cost:
-            print("You don't have enough money!")
-            return
     elif airport_type == "medium_airport":
         terminal_id = 1
-        terminal_cost = 6_000_000
-        if balance < terminal_cost:
-            print("You don't have enough money!")
-            return
     elif airport_type == "large_airport":
         terminal_id = 2
-        terminal_cost = 12_000_000
-        if balance < terminal_cost:
-            print("You don't have enough money!")
-            return
+
+    cursor = connect.cursor(dictionary=True)
+    cursor.execute("select size, cost, construction_time, operating_cost, yield from terminal_types where id=%s",
+                   (terminal_id,))
+    terminal = cursor.fetchone() #terminaalin tiedot sanakirjana, sarakkeen nimet ovat sanakirjan avaimia
+
+    if balance < terminal["cost"]:
+        print("You don't have enough money!")
+        return
     airport_id = get_airport_id(connect, player, icao)
-    cursor = connect.cursor()
     cursor.execute("""
                    insert into player_terminal (player_airports_id, terminal_types_id)
                    values (%s, %s)
                    """, (airport_id, terminal_id))
-    cursor.execute("update game set money = money - %s where name = %s", (terminal_cost, player,))
+    #Päivitetään player_airports.yield
+    sum_yield = terminal["yield"]- terminal["operating_cost"]
+    cursor.execute("UPDATE player_airports SET yield = yield + %s WHERE id = %s", (sum_yield, airport_id))
+    cursor.execute("update game set money = money - %s where name = %s", (terminal["cost"], player,))
     print("Terminal purchase successful!")
     cursor.close()
