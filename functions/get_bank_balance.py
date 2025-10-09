3892ea46e21ff2b1e7f031567ebbccfd37dd17c9
eetu <eetu.vaananen@metropolia.fi>
2025-10-09 20:39:14 +0300

    give_bank_balance antaa nyt myös pelaajan kaikkien terminaalien ja kiitoratojen yieldin

--- functions/get_bank_balance.py
+++ functions/get_bank_balance.py
@@ -3,5 +3,26 @@ def give_bank_balance(connect, player):
     cursor = connect.cursor(buffered=True)
     cursor.execute(sql, (player,))
     balance = cursor.fetchone()
+
+    sql_terminal_yield = """SELECT SUM(terminal_types.yield) FROM terminal_types
+    INNER JOIN player_terminal ON player_terminal.terminal_types_id = terminal_types.id
+    INNER JOIN player_airports ON player_terminal.player_airports_id = player_airports.id
+    INNER JOIN game ON game.player_airports_id = player_airports.id
+    WHERE game.name = %s"""
+    cursor.execute(sql_terminal_yield, (player,))
+    terminal_yield = cursor.fetchone()
+
+    sql_runway_yield = """SELECT SUM(runway_types.yield)  FROM runway_types
+    INNER JOIN player_runway ON player_runway.runway_types_id = runway_types.id
+    INNER JOIN player_airports ON player_runway.player_airports_id = player_airports.id
+    INNER JOIN game ON game.player_airports_id = player_airports.id
+    WHERE game.name = %s"""
+    cursor.execute(sql_runway_yield, (player,))
+    runway_yield = cursor.fetchone()
+
+    total_yield = runway_yield + terminal_yield
+
     cursor.close()
-    return balance[0]
+    return balance[0], total_yield
+
+
