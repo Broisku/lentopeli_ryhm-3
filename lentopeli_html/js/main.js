0cd7710e59b42792fb68251da753aacc532d3785
ptpar <pinja.t.parviainen@gmail.com>
2025-12-01 17:45:08 +0200

    Add files via upload

---
+++ lentopeli_html/js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const map = L.map('map').setView([52.52, 13.4], 4);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
