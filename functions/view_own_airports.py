e93b54fcf5a81f6e13980c2e68639dc6f6fec81b
Broisku <miro.o.heiskanen@gmail.com>
2025-12-06 17:15:04 +0200

    fix

--- functions/view_own_airports.py
+++ functions/view_own_airports.py
@@ -5,7 +5,8 @@ select type, airport.name, municipality, gps_code, iata_code, elevation_ft, lati
 from airport
 join player_airports
 on airport.player_airports_id = player_airports.id
-join game on game.name = %s
+join game on player_airports.game_id = game.id
+where game.name = %s
 """, (player,))
     airports = cursor.fetchall()
     return airports
