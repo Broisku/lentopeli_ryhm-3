53679d2422ca0ad9c1575339e28ddfe2369ecf76
Broisku <miro.o.heiskanen@gmail.com>
2025-12-07 19:07:23 +0200

    buy nappula

--- lentopeli_html/js/main.js
+++ lentopeli_html/js/main.js
@@ -13,14 +13,41 @@ fetch("http://127.0.0.1:5000/airports")
   airports.forEach(airport => {
 
     const name = airport[3];
+    const type = airport[2];
     const lat = parseFloat(airport[4]);
     const lon = parseFloat(airport[5]);
+    const country = airport[8];
+    const municipality = airport[10];
+    const iata = airport[12];
+    const icao = airport[11];
 
     if (!isNaN(lat) && !isNaN(lon)) {
-      L.marker([lat, lon])
-          .addTo(map)
-          .bindPopup(`<b>${airport.name}</b>`);
-    }
-  })
+
+      const marker = L.marker([lat, lon]).addTo(map);
+
+          marker.bindPopup(`
+            <b>${name}</b><br>
+            Type: ${type}<br>
+            City: ${municipality}<br>
+            Country: ${country}<br>
+            IATA: ${iata || "N/A"}<br>
+            ICAO: ${icao || "N/A"}<br>
+            Lat: ${lat}<br>
+            Lon: ${lon}
+            
+            <button class="buyBtn">Buy</button>
+          `)
+
+          marker.on("popupopen", (e) => {
+            const btn = e.popup.getElement().querySelector(".buyBtn");
+
+            btn.addEventListener("click", () => {
+              alert(`Airport purchased: ${name}`);
+              console.log({ name, iata, icao, lat, lon });
+        });
+      })
+
+  }
+})
 })
 .catch(error => console.error(error));
