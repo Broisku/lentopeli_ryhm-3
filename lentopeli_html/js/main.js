f932dac25d9c00da88b8b41a8ddce267ec913a15
Broisku <miro.o.heiskanen@gmail.com>
2025-12-07 04:08:59 +0200

    lentokentät näkyy nyt kartalla

--- lentopeli_html/js/main.js
+++ lentopeli_html/js/main.js
@@ -5,3 +5,22 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// haetaan lentokentät python apista
+fetch("http://127.0.0.1:5000/airports")
+.then(res => res.json())
+.then(airports => {
+  airports.forEach(airport => {
+
+    const name = airport[3];
+    const lat = parseFloat(airport[4]);
+    const lon = parseFloat(airport[5]);
+
+    if (!isNaN(lat) && !isNaN(lon)) {
+      L.marker([lat, lon])
+          .addTo(map)
+          .bindPopup(`<b>${airport.name}</b>`);
+    }
+  })
+})
+.catch(error => console.error(error));
